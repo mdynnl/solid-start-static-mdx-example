2bde9271d003e71e39694d585256964d4b95cf81
Nyi Nyi Lwin <mdy.nnl@gmail.com>
2023-03-19 21:19:22 +0630

    base

--- assets/_...404_-0b850961.js
+++ assets/_...404_-0b850961.js
@@ -1,2 +1,2 @@
-import{M as r}from"./index-a96602bc.js";import{c as n,m as c,A as s}from"./entry-client-3ca96fdd.js";function o(t){const e=Object.assign({h1:"h1",p:"p",a:"a"},r(),t.components);return[n(e.h1,{children:"Page Not Found"}),`
+import{M as r}from"./index-3fe20828.js";import{c as n,m as c,A as s}from"./entry-client-358052b1.js";function o(t){const e=Object.assign({h1:"h1",p:"p",a:"a"},r(),t.components);return[n(e.h1,{children:"Page Not Found"}),`
 `,n(e.p,{get children(){return["Visit ",n(e.a,{href:"https://solidjs.com",children:"https://solidjs.com"})," to learn how to build Solid apps. ",n(s,{href:"/docs",children:"Docs"})]}})]}function d(t={}){const{wrapper:e}=Object.assign({},r(),t.components);return e?n(e,c(t,{get children(){return n(o,t)}})):o(t)}export{d as default};
